d0434d31999ab8f947a23213a643d29f30cf13e7
Kush Aynstayn <njarado2001@gmail.com>
2025-05-13 12:37:38 +0800

    chatapp class

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ChatApp());
+}
+
+class ChatApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw Text('Hello World!');
+  }
+
+}
+
+
+
+// TODO Create ChatApp widget
+// TODO Create LoginPage widget
