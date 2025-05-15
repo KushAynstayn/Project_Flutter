b08e94e3befa3df16e584e447d41becee81daf9b
Kush Aynstayn <njarado2001@gmail.com>
2025-05-15 23:58:12 +0800

    icon button

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -83,6 +83,32 @@ class ChatPage extends StatelessWidget {
             ),
           ),
 
+
+          Container(
+            height: 100,
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: (){}, 
+                  icon: Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  )
+                ),
+                 IconButton(
+                  onPressed: (){}, 
+                  icon: Icon(
+                    Icons.send,
+                    color: Colors.white,
+                  )
+                )
+              ],
+            ),
+            decoration: BoxDecoration(
+              color: Colors.purple,
+              borderRadius: BorderRadius.vertical(top: Radius.circular(20))
+            ),
+          )
         ],
       ),
     );
