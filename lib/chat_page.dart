0cd58a6f1b728d0b6b7e2b1924bab02985e582d4
Kush Aynstayn <njarado2001@gmail.com>
2025-05-16 22:43:52 +0800

    custom widget

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:project_flutter/widgets/chat_bubble.dart';
+import 'package:project_flutter/widgets/chat_input.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
@@ -25,108 +27,17 @@ class ChatPage extends StatelessWidget {
           Expanded(
             child: ListView(
               children: [
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Container(
-                    padding: EdgeInsets.all(24),
-                    child: Column(
-                      
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text('Hi, this is your message!', style: TextStyle(fontSize: 20, color: Colors.white )),
-                        Image.network('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif', height: 200, width: 200
-                        ,)
-                      ],
-                    ),
-                    margin: EdgeInsets.all(50),
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(12),
-                        topRight: Radius.circular(12),
-                        bottomLeft: Radius.circular(12)
-                      )
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Container(
-                    padding: EdgeInsets.all(24),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text('Hi, this is your message!', style: TextStyle(fontSize: 20, color: Colors.white )),
-                        Image.network('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif', height: 200, width: 200
-                        ,)
-                      ],
-                    ),
-                    margin: EdgeInsets.all(50),
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(12),
-                        topRight: Radius.circular(12),
-                        bottomLeft: Radius.circular(12)
-                      )
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Container(
-                    padding: EdgeInsets.all(24),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text('Hi, this is your message!', style: TextStyle(fontSize: 20, color: Colors.white )),
-                        Image.network('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif', height: 200, width: 200
-                        ,)
-                      ],
-                    ),
-                    margin: EdgeInsets.all(50),
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(12),
-                        topRight: Radius.circular(12),
-                        bottomLeft: Radius.circular(12)
-                      )
-                    ),
-                  ),
-                ),
-            
+               ChatBubble(alignment: Alignment.centerLeft, 
+               message:  'Hello, this is Aynstayn!'),
+               ChatBubble(alignment: Alignment.centerRight, 
+               message:  'Yooooo!'),
+               
             
                 
               ],
             ),
           ),
-          Container(
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    IconButton(
-                      onPressed: (){}, 
-                      icon: Icon(
-                        Icons.add,
-                        color: Colors.white,
-                      )
-                    ),
-                     IconButton(
-                      onPressed: (){}, 
-                      icon: Icon(
-                        Icons.send,
-                        color: Colors.white,
-                      )
-                    )
-                  ],
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.purple,
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(20))
-                ),
-              )
+          ChatInput(),
         ],
       ),
     );
