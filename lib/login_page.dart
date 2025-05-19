2875b8d191dec80123aab1f2900a0ee2fef29b26
Kush Aynstayn <njarado2001@gmail.com>
2025-05-19 22:33:28 +0800

    inputs from user

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+   LoginPage({super.key});
 
   void loginUser() {
+    print(userNameController.text);
+    print(passwordController.text);
     print('login successful!');
   }
+
+  final userNameController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -43,11 +48,31 @@ class LoginPage extends StatelessWidget {
                 height: 200,
               ),
 
+              TextField(
+                controller: userNameController,
+                decoration: InputDecoration(
+                  hintText: 'Add your username',
+                  hintStyle: TextStyle(color: Colors.blueGrey),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Add your password',
+                  hintStyle: TextStyle(color: Colors.blueGrey),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 16)),
+
               ElevatedButton(
                 onPressed: loginUser,
                 child: Text(
-                  'Click me!',
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w300),
+                  'Login',
+                  style: TextStyle(fontSize: 23, fontWeight: FontWeight.w300),
                 ),
               ),
               InkWell(
