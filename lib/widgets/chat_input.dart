70a2a68f6d3957d34d346cf6d8e4d857a38f9869
Kush Aynstayn <njarado2001@gmail.com>
2025-05-26 15:40:07 +0800

    showing the selected image in ChatInput

--- lib/widgets/chat_input.dart
+++ lib/widgets/chat_input.dart
@@ -2,9 +2,17 @@ import 'package:project_flutter/models/chat_message_entity.dart';
 import 'package:project_flutter/widgets/picker_body.dart';
 import 'package:flutter/material.dart';
 
-class ChatInput extends StatelessWidget {
+class ChatInput extends StatefulWidget {
   final Function(ChatMessageEntity) onSubmit;
   ChatInput({super.key, required this.onSubmit});
+
+   @override
+   State<ChatInput> createState() => _ChatInputState();
+ }
+
+class _ChatInputState extends State<ChatInput> {
+  String _selectedImageUrl = '';
+
 
 
   final chatMessageController = TextEditingController();
@@ -18,13 +26,26 @@ class ChatInput extends StatelessWidget {
         createdAt: DateTime.now().millisecondsSinceEpoch,
         author: Author(userName: 'aynstayn'));
 
-    onSubmit(newChatMessage);
+     //TODO: Check for image and append it to the chat object
+    if (_selectedImageUrl.isNotEmpty) {
+      newChatMessage.imageUrl = _selectedImageUrl;
+    }
+    widget.onSubmit(newChatMessage);
+    chatMessageController.clear();
+    _selectedImageUrl = '';
+    setState(() {});
+  }
+
+  void onImagePicked(String newImageUrl) {
+    setState(() {
+      _selectedImageUrl = newImageUrl;
+    });
+    Navigator.of(context).pop();
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100,
       decoration: BoxDecoration(
         color: Colors.black,
         borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
@@ -38,24 +59,35 @@ class ChatInput extends StatelessWidget {
               showModalBottomSheet(
                   context: context,
                   builder: (BuildContext context) {
-                    return NetworkImagePickerBody();
+                    return NetworkImagePickerBody(
+                      onImageSelected: onImagePicked,
+                    );
                   });
             },
             icon: Icon(Icons.add, color: Colors.white),
           ),
           Expanded(
-            child: TextField(
-              keyboardType: TextInputType.multiline,
-              maxLines: 5,
-              minLines: 1,
-              controller: chatMessageController,
-              textCapitalization: TextCapitalization.sentences,
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                hintText: "Type your message",
-                hintStyle: TextStyle(color: Colors.blueGrey),
-                border: InputBorder.none,
+            child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: 5,
+                minLines: 1,
+                controller: chatMessageController,
+                textCapitalization: TextCapitalization.sentences,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                    hintText: "Type your message",
+                    hintStyle: TextStyle(color: Colors.blueGrey),
+                    border: InputBorder.none),
               ),
+              if (_selectedImageUrl.isNotEmpty)
+                Image.network(
+                  _selectedImageUrl,
+                  height: 50,
+                ),
+            ],
             ),
           ),
           IconButton(
